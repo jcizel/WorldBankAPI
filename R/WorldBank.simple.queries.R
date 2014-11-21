51e2f34b01e48678a018ac7fdb30aca59d09e386
jcizel <jcizel@gmail.com>
2014-11-21 15:08:05 +0100

    new functionality

--- R/WorldBank.simple.queries.R
+++ R/WorldBank.simple.queries.R
@@ -28,10 +28,83 @@ queryWorldBankVariableList <- function(pattern = "") {
         v <- WorldBankAPI:::getWorldBankListOfVariables()
     }
 
-    out <- v[toupper(sourceNote) %like% toupper(pattern)][,list(source.id,id,name)]
+    out <- v[ (toupper(name) %like% toupper(pattern))
+             |(toupper(sourceNote) %like% toupper(pattern))][,list(source.id,id,name)]
 
     return(out)
 }
+
+getWorldBankDataSeries <- function(
+    url="http://api.worldbank.org"
+   ,indicators = c('')
+   ,countries = "all"
+   ,MRV = "100"
+   ,format = "json"
+   ,per_page = "10000"
+){
+    result.list <- 
+        foreach(x = indicators,
+                .errorhandling = 'stop') %dopar%
+    {
+        cat(x,"\n")
+        
+        string<- paste(url
+                      ,"countries", paste(countries,sep=";")
+                      ,"indicators", x
+                      ,sep = "/"
+                       )
+
+        query <- paste0(string,
+                        "?",
+                        "format=",format,
+                        "&per_page=",per_page
+                        )
+
+        cat(query,"\n")
+        raw.data <- try(fromJSON(query))
+
+        if ("try-error" %in% class(raw.data)){
+            warning(x, " failed to load.")
+            next
+        } else {
+            if (raw.data[[1]][["pages"]] <= 1){
+                data <- raw.data[[2]]
+            } else {
+                data <- raw.data[[2]]
+                for (y in 2:raw.data[[1]][["pages"]]){
+                    info <- raw.data[[1]]
+                    paste(str(info))
+                    raw.data <- try(fromJSON(paste0(query,"&page=",y)))
+                    if ("try-error" %in% class(raw.data)){
+                        warning(x, " page ", y , " failed to load.")
+                        next
+                    } else {
+                        data <- c(data,raw.data[[2]])
+                    }
+                }
+            }
+        }            
+
+        dat <- lapply(data, function(l){
+            as.data.table(as.list(unlist(l)))
+        })
+
+        dat <- rbindlist(dat, fill = TRUE)                        
+        
+        dat
+    }
+    
+    .r <- rbindlist(result.list, fill = TRUE, use.names = TRUE)
+
+    ## POST-PROCESSING
+    .r[, value := as.numeric(value)]
+    .r[, date := as.Date(paste0(date,"-12-31"),"%Y-%m-%d")]
+
+    result <- .r[!is.na(value)]
+    
+    return(result)
+}
+
 
 ## queryWorldBankVariableList("debt")
 ## queryWorldBankVariableList("gdp")
--- scripts/examples.R
+++ scripts/examples.R
@@ -1,5 +1,9 @@
 load_all()
 
+cl <- parallel:::makeCluster(8)
+doParallel:::registerDoParallel(cl)
+
+
 sources <- WorldBankAPI:::getWorldBankSources()
 
 ctry <- WorldBankAPI:::getWorldBankCountries()
@@ -9,29 +13,12 @@ WorldBankAPI:::getWorldBankDataset(source = '11')
 
 ## CREATE A TABLE WITH ALL AVAILABLE VARIABLES
 
-require(foreach)
-require(doParallel)
-listAllFun('doParallel')
-listAllFun('parallel')
 
-cl <- parallel:::makeCluster(8)
-doParallel:::registerDoParallel(cl)
+indicators <- queryWorldBankVariableList('gross domestic product')
+o <- getWorldBankDataSeries(indicators = indicators[,id])
 
-
-sources <- getWorldBankSources()[,list(id,name)]
-
-out <-
-    foreach(x = sources$id,
-            .errorhandling = 'pass') %dopar% {
-        o <- WorldBankAPI:::getWorldBankIndicatorList(source = x)
-        out <- o[, list(id, name, source.id, source.value, sourceNote)]
-    }
-
-
-out <- Filter(function(d) ('data.table' %in% class(d)), out)
-lookup <- rbindlist(out)
-
-lookup[toupper(name) %like% toupper('debt')][,list(source.id,id,name)]
+o[,table(country.value)]
+o[,table(date)]
 
 
 
